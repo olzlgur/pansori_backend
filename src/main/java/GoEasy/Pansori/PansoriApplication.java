d50a897df63db8ecb5434b79875b71e2ab2924ec
이지혁 <olzlgur@naver.com>
2022-07-06 20:29:13 +0900

    project setting

---
+++ src/main/java/GoEasy/Pansori/PansoriApplication.java
@@ -0,0 +1,13 @@
+package GoEasy.Pansori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PansoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PansoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GoEasy/Pansori/PansoriApplicationTests.java
@@ -0,0 +1,13 @@
+package GoEasy.Pansori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PansoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
