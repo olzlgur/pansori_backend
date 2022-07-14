8317f94dc3c74080a8b8c8d616507b4f53310914
이지혁 <olzlgur@naver.com>
2022-07-14 19:19:26 +0900

    인코딩 라이브러리 추가

--- src/main/java/GoEasy/Pansori/PansoriApplication.java
+++ src/main/java/GoEasy/Pansori/PansoriApplication.java
@@ -2,12 +2,23 @@ package GoEasy.Pansori;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.util.logging.Filter;
 
 @SpringBootApplication
 public class PansoriApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PansoriApplication.class, args);
+	}
+	@Bean
+	public CharacterEncodingFilter characterEncodingFilter() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		return characterEncodingFilter;
 	}
 
 }
