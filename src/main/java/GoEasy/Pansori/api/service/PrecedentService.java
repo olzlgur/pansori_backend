083d352079187fe600b8df16f3774187837d8502
이지혁 <olzlgur@naver.com>
2022-08-08 16:10:23 +0900

    검색 api 구현

---
+++ src/main/java/GoEasy/Pansori/api/service/PrecedentService.java
@@ -0,0 +1,76 @@
+package GoEasy.Pansori.api.service;
+
+import GoEasy.Pansori.api.domain.DetailPrecedent;
+import GoEasy.Pansori.api.domain.SimplePrecedent;
+import GoEasy.Pansori.api.dto.PrecedentApiDto;
+import GoEasy.Pansori.api.dto.PrecedentDto;
+import GoEasy.Pansori.api.dto.PrecedentInsertDto;
+import GoEasy.Pansori.api.dto.PrecedentListDto;
+import GoEasy.Pansori.api.repository.PrecedentRepository;
+import GoEasy.Pansori.exception.customException.CustomTypeException;
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
+import kr.co.shineware.nlp.komoran.model.Token;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PrecedentService {
+    private final PrecedentRepository precedentRepository;
+
+    public DetailPrecedent findOne(Long precedent_id) {
+        return precedentRepository.findOne(precedent_id);
+    }
+
+
+
+    public PrecedentApiDto findOnePrecedent(Long id) {
+        DetailPrecedent detailPrecedent = precedentRepository.findOne(id);
+        PrecedentApiDto precedentApiDto = PrecedentApiDto.PrecedentApiDto(detailPrecedent);
+
+        return precedentApiDto;
+    }
+
+
+    public PrecedentListDto searchAccuracy(String content){
+        PrecedentListDto precedentListDto = precedentRepository.searchAccuracy(morphemeAnalysis(content));
+        if (precedentListDto.getPrecedentDtoList().size() == 0){
+            throw new CustomTypeException("검색 결과가 없습니다.");
+        }
+        return precedentListDto;
+    }
+
+    public PrecedentListDto searchRecent(String content){
+        return precedentRepository.searchRecent(morphemeAnalysis(content));
+    }
+
+
+    public List<String> morphemeAnalysis(String searchContent) {
+        Komoran korman = new Komoran(DEFAULT_MODEL.FULL);
+        List<String> contents = new ArrayList<>();
+
+        KomoranResult analyzeResultList = korman.analyze(searchContent);
+
+        System.out.println(analyzeResultList.getPlainText());
+
+        List<Token> tokenList = analyzeResultList.getTokenList();
+
+        for (Token token : tokenList) {
+            if(token.getPos().contains("NN")){
+                contents.add(token.getMorph());
+            }
+        }
+        if (contents.size() == 0){
+            throw new CustomTypeException("검색 결과가 없습니다.");
+        }
+        return contents;
+    }
+
+}
