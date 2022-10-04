c6a98310d9e53469d70b0eb3d01b2f45ce99db8b
DeveloperHunny <eogns0321@gmail.com>
2022-10-04 21:19:16 +0900

    비밀번호 인증 메일 양식 완료

--- src/main/java/GoEasy/Pansori/service/MailSendService.java
+++ src/main/java/GoEasy/Pansori/service/MailSendService.java
@@ -2,14 +2,16 @@ package GoEasy.Pansori.service;
 
 import GoEasy.Pansori.dto.MailDto;
 import GoEasy.Pansori.exception.customException.CustomTypeException;
+import GoEasy.Pansori.utils.MailUtils;
 import lombok.AllArgsConstructor;
-//import org.springframework.mail.
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.UnsupportedEncodingException;
 import java.util.Random;
 
 @Service
@@ -19,14 +21,45 @@ public class MailSendService {
     private JavaMailSender javaMailSender;
     private static final String from_address = "olzlgur@naver.com";
     private static final String text = "이메일 인증 코드";
+
     public String mailSend(MailDto mailDto, HttpServletRequest request){
         String toEmail = mailDto.getEmail();
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setFrom(MailSendService.from_address);
-        message.setSubject(text);
-        message.setText("판소리 회원가입 인증번호 \n" + createAuthKey(toEmail, request));
-        javaMailSender.send(message);
+
+        //인증메일 보내기
+        try {
+            //메세지 메타데이터 설정
+            MailUtils sendMail = new MailUtils(javaMailSender);
+            sendMail.setSubject(text);
+            sendMail.setFrom(MailSendService.from_address, "관리자");
+            sendMail.setTo(toEmail);
+
+
+
+                    //메세지 내용 설정
+            String msg = "";
+            msg += "<div style=\"width: 50%; padding:36px 24px\">";
+            msg += "<h1 style=\"font-size:36px;\"> 이메일 인증 코드</h1>";
+            msg += "<br>";
+            msg += "<div>";
+            msg += "        <p>" + toEmail + "님, 안녕하세요.</p>\n" +
+                    "        <p>귀하의 이메일 주소를 통해 Pansori 계정에 대한 비밀번호 찾기가 요청되었습니다.</p>\n" +
+                    "        <p>Pansori 인증 코드는 다음과 같습니다.</p>\n" +
+                    "\n" +
+                    "        <p style=\"background-color: rgba(0, 0, 0, 0.1); font-size: 48px; padding:8px 24px;\">" +  createAuthKey(toEmail, request) +  "</p>\n" +
+                    "\n" +
+                    "        <p>이 코드를 요청하지 않았다면 다른 사람이 Pansori 계정 " + toEmail + "에 액세스하려고 시도하는 것일 수 있습니다.</p>\n" +
+                    "        <p>누구에게도 이 코드를 전달하거나 제공하면 안 됩니다.</p>\n" +
+                    "        <p>Pansori 계정팀</p>";
+            msg += "</div></div>";
+            sendMail.setText(msg);
+
+            //메세지 전송
+            sendMail.send();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
         return "메일 전송";
     }
