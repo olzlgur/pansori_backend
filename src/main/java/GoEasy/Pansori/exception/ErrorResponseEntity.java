21e161e11760d7271eead3b18b4aba2534df91b3
DeveloperHunny <eogns0321@gmail.com>
2022-11-11 10:51:09 +0900

    로그인 인증 에러 추가

--- src/main/java/GoEasy/Pansori/exception/ErrorResponseEntity.java
+++ src/main/java/GoEasy/Pansori/exception/ErrorResponseEntity.java
@@ -2,7 +2,9 @@ package GoEasy.Pansori.exception;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 
 @Data
 @Builder
@@ -18,6 +20,26 @@ public class ErrorResponseEntity {
                         .status(e.getHttpStatus().value())
                         .code(e.getHttpStatus().name())
                         .message(e.getMsg())
+                        .build());
+    }
+
+    public static ResponseEntity<ErrorResponseEntity> toResponseEntity(BadCredentialsException e){
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponseEntity.builder()
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .code(HttpStatus.BAD_REQUEST.name())
+                        .message("Invalid email or password")
+                        .build());
+    }
+
+    public static ResponseEntity<ErrorResponseEntity> toResponseEntity(Exception e){
+        return ResponseEntity
+                .status(ErrorCode.UNKNOWN_ERROR.getHttpStatus())
+                .body(ErrorResponseEntity.builder()
+                        .status(ErrorCode.UNKNOWN_ERROR.getHttpStatus().value())
+                        .code(ErrorCode.UNKNOWN_ERROR.name())
+                        .message("알 수 없는 오류가 발생했습니다.")
                         .build());
     }
 }
