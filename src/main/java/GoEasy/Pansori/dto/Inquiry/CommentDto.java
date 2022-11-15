04e5b6ef568e027fdf14e2285f570be1b974a16e
DeveloperHunny <eogns0321@gmail.com>
2022-11-15 11:11:27 +0900

    Comment Service 로직 추가 및 API 추가

--- src/main/java/GoEasy/Pansori/dto/Inquiry/CommentDto.java
+++ src/main/java/GoEasy/Pansori/dto/Inquiry/CommentDto.java
@@ -1,2 +1,30 @@
-package GoEasy.Pansori.dto.Inquiry;public class CommentDto {
+package GoEasy.Pansori.dto.Inquiry;
+
+import GoEasy.Pansori.domain.Inquiry.Comment;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class CommentDto {
+
+    private Long id;
+    private Long writer_id;
+    private Long inquiry_id;
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public static CommentDto createDto(Comment comment){
+        CommentDto dto = new CommentDto();
+        dto.id = comment.getId();
+        dto.writer_id = comment.getWriter().getId();
+        dto.inquiry_id = comment.getInquiry().getId();
+        dto.content = comment.getContent();
+        dto.createdDate = comment.getCreatedDate();
+        dto.modifiedDate = comment.getModifiedDate();
+        return dto;
+    }
 }
--- src/main/java/GoEasy/Pansori/dto/Inquiry/CommentUpdateRequestDto.java
+++ src/main/java/GoEasy/Pansori/dto/Inquiry/CommentUpdateRequestDto.java
@@ -1,13 +1,10 @@
 package GoEasy.Pansori.dto.Inquiry;
 
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class CommentCreateRequestDto {
-
-    private Long writer_id;
+public class CommentUpdateRequestDto {
     private String content;
 }
