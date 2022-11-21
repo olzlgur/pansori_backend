e4b9c905eced9ca1f67b3760f9532e3be04bc3d1
hoony0321 <eogns0321@gmail.com>
2022-11-21 17:01:14 +0900

    refactoring : ErrorCode 추가

--- src/main/java/GoEasy/Pansori/exception/ErrorCode.java
+++ src/main/java/GoEasy/Pansori/exception/ErrorCode.java
@@ -19,7 +19,9 @@ public enum ErrorCode {
 
     INVALID_JWT(HttpStatus.BAD_REQUEST, "올바른 JWT 토큰이 아닙니다."),
 
-    NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "해당 URL에 대한 접근 권한이 없습니다.")
+    NOT_ACCEPTABLE(HttpStatus.NOT_ACCEPTABLE, "해당 URL에 대한 접근 권한이 없습니다."),
+
+    TOKEN_ERROR(HttpStatus.UNAUTHORIZED, "토큰 처리 과정 중에 에러가 발생했습니다.")
     ;
 
     private final HttpStatus httpStatus;
